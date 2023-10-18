961cc1cbd13624195ad20fb5dd319e07290ba38d
Neha <ravuri.n@northeastern.edu>
2023-10-18 17:47:05 -0400

    modules

--- kanbas-react-web-app/src/Kanbas/Courses/Modules/ModuleList.js
+++ kanbas-react-web-app/src/Kanbas/Courses/Modules/ModuleList.js
@@ -1,29 +1,33 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import db from "../../Database";
-import "./index.css"
+import "./index.css";
+import { FaCheckCircle } from "react-icons/fa";
+import { AiFillCaretDown, AiOutlinePlus } from "react-icons/ai";
+import { HiEllipsisVertical } from "react-icons/hi2";
 
 function ModuleList() {
   const { courseId } = useParams();
   const modules = db.modules;
   return (
-    <ul className="list-group py-5">
-      {modules
-        .filter((module) => module.course === courseId)
-        .map((module, index) => (
-          <li key={index} className="list-group-item modules my-2">
-            <h3>{module.name}</h3>
-            <p>{module.description}</p>
-            {module.lessons &&
-              module.lessons.map((lesson, index) => (
-                <div key={index}>
-                  <p>{lesson.name}</p>
-                  <p>{lesson.description}</p>
-                </div>
-              ))}
-          </li>
-        ))}
-    </ul>
+    <div>
+      <ul className="list-group my-4">
+        {modules
+          .filter((module) => module.course === courseId)
+          .map((module, index) => (
+            <li key={index} className="list-group-item modules my-3">
+              <div>
+                {module.name}
+                <HiEllipsisVertical className= "float-end"/>
+                <AiOutlinePlus className= "float-end"/>
+
+                <FaCheckCircle className= "float-end" color="green" />
+                <AiFillCaretDown className= "float-end" />
+              </div>
+            </li>
+          ))}
+      </ul>
+    </div>
   );
 }
 export default ModuleList;
