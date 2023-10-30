a79185c277df240ce92361a32dcb987dc2cb13da
Neha <ravuri.n@northeastern.edu>
2023-10-30 16:26:15 -0400

    a4 labs

--- kanbas-react-web-app/src/Labs/a3/index.js
+++ kanbas-react-web-app/src/Labs/a3/index.js
@@ -2,14 +2,22 @@ import JavaScript from "./JavaScript";
 import DynamicStyling from "./DynamicStyling";
 import ConditionalOutput from "./ConditionalOutput";
 import TodoList from "./todos/TodoList";
+import { useSelector } from "react-redux";
 function Assignment3() {
+  const { todos } = useSelector((state) => state.todosReducer);
   return (
     <div>
-      <h1>Assignment 3</h1>
-      <TodoList/>
-      <ConditionalOutput/>
+      <h2>Assignment 3</h2>
+      <ul className="list-group">
+        {todos.map((todo) => (
+          <li className="list-group-item" key={todo.id}>
+            {todo.title}
+          </li>
+        ))}
+      </ul>
+      <ConditionalOutput />
       <JavaScript />
-      <DynamicStyling/>
+      <DynamicStyling />
     </div>
   );
 }
